283eae0dc0b330f4c025b93eac8809b7e25ffa5b
Tianshu <zhutianshuuoft@gmail.com>
2017-09-26 16:42:51 -0400

    finished divide part

--- 411_a1/q1.py
+++ 411_a1/q1.py
@@ -1,6 +1,7 @@
 from sklearn import datasets
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 # used to load the data
 def load_data():
@@ -22,7 +23,7 @@ def visualize(X, y, features):
         plt.subplot(3, 5, i + 1)
         #TODO: Plot feature i against y
         # 'ro' to plot as red circle
-        plt.plot(X[:, i], y, 'ro')
+        plt.plot(X[:, i], y, '.')
         plt.title(features[i])
         plt.ylabel('target')
     
@@ -33,28 +34,38 @@ def visualize(X, y, features):
 def fit_regression(X,Y):
     #TODO: implement linear regression
     # Remember to use np.linalg.solve instead of inverting!
-    raise NotImplementedError()
+    
+    # get a randomed array of indices
+    data_count = Y.shape[0]
+    permutation = np.random.permutation(data_count)
+    
+    # divide into 80%, 20%
+    breakPoint = math.ceil(data_count*0.8)
+    trainingIndices = permutation[0 : breakPoint]
+    testingIndices = permutation[breakPoint :]
+    training_x = numpy.take(a, indices, axis=None, out=None, mode='raise')[source]
+    
+    
+    # compute linear regression
+    # w = numpy.linalg.solve(a, b)
 
 def main():
     # Load the data
     X, y, features = load_data()
-    """
-    print(type(X))
-    print(type(y))
-    print(type(features))
-    print(X[1])
-    print("X: {}".format(X))
-    print("y: {}".format(y))
-    """
+    
+    '''
     print("Features: {}".format(features))
+    '''
     
     # Visualize the features
+    '''
     visualize(X, y, features)
-
+    '''
+    
     #TODO: Split data into train and test
 
     # Fit regression model
-    #w = fit_regression(X, y)
+    w = fit_regression(X, y)
 
     # Compute fitted values, MSE, etc.
 
